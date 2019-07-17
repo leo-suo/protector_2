c1d62b213b8905f778bf99cad7832602f4fac3a4
suoyinglun <y3suo@uwaterloo.ca>
2019-07-17 18:00:13 -0400

    add summon view and video

--- app/src/main/java/com/example/protector_2/Summon_Activity.java
+++ app/src/main/java/com/example/protector_2/Summon_Activity.java
@@ -1,7 +1,12 @@
 package com.example.protector_2;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Summon_Activity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class Summon_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summon_);
+        VideoView v = findViewById(R.id.videoView);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.videoplayback;
+        Uri uri = Uri.parse(path);
+        v.setVideoURI(uri);
+    }
+
+    public void onPlayClick(View view){
+        Button play = findViewById(R.id.play_bt);
+        if(play.getText() == "Play"){
+            VideoView videoView = findViewById(R.id.videoView);
+            videoView.start();
+            play.setText("Stop");
+        } else{
+            VideoView videoView = findViewById(R.id.videoView);
+            videoView.pause();
+            play.setText("Play");
+        }
+
     }
 }
--- app/src/main/java/com/example/protector_2/shop_Activity.java
+++ app/src/main/java/com/example/protector_2/shop_Activity.java
@@ -1,10 +1,14 @@
 package com.example.protector_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class shop_Activity extends AppCompatActivity {
     public int gold_num;
@@ -14,8 +18,8 @@ public class shop_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_);
-        gold_num = 0;
-        TextView t = findViewById(R.id.Gold);
+        read_gold_files();
+        TextView t = findViewById(R.id.Gold_text);
         t.setText("Gold :" +String.valueOf(gold_num));
     }
 
@@ -23,11 +27,36 @@ public class shop_Activity extends AppCompatActivity {
         finish();
     }
 
+    public void onSummonClick(View View){
+        Intent intent = new Intent(shop_Activity.this,
+                Summon_Activity.class);
+        startActivity(intent);
+    }
+
     public void onPurchaseClick(View View){
         String num = String.valueOf(gold_num+100);
-        gold_num+=100;
-        TextView t = findViewById(R.id.Gold);
+        gold_num-=100;
+        TextView t = findViewById(R.id.Gold_text);
         t.setText("Gold: "+num );
+    }
+
+
+    public void read_gold_files(){
+        String data = "";
+        StringBuffer sbuffer = new StringBuffer();
+        InputStream is = this.getResources().openRawResource(R.raw.gold_info);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if (is != null) {
+            try {
+                while ((data = reader.readLine()) != null) {
+                    sbuffer.append(data);
+                }
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        gold_num = Integer.parseInt(sbuffer.toString());
     }
 
 }
