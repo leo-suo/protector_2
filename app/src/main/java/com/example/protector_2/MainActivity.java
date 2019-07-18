1ff654d18f79315af935cbe0aa2422c52581f4f1
alberttang <w54tang@edu.uwaterloo.ca>
2019-07-18 18:22:26 -0400

    Finish Decorator

--- app/src/main/java/com/example/protector_2/MainActivity.java
+++ app/src/main/java/com/example/protector_2/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button play, bag, shop, item;
+    private Button play, bag, shop;
 
 
     @Override
