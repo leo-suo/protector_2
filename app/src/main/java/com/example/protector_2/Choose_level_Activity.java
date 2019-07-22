5c0d57306fe81452a0a7a6e3e7c6a781c734d18b
alberttang <w54tang@edu.uwaterloo.ca>
2019-07-22 01:57:02 -0400

    we will winnnnnn x2

--- app/src/main/java/com/example/protector_2/Choose_level_Activity.java
+++ app/src/main/java/com/example/protector_2/Choose_level_Activity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class Choose_level_Activity extends AppCompatActivity {
@@ -19,6 +21,8 @@ public class Choose_level_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_choose_level_);
 
         //load some information
--- app/src/main/java/com/example/protector_2/Summon_Activity.java
+++ app/src/main/java/com/example/protector_2/Summon_Activity.java
@@ -22,6 +22,44 @@ public class Summon_Activity extends AppCompatActivity {
 
     public void onPlayClick(View view){
         Button play = findViewById(R.id.play_bt);
+        int randomnum = (int) (Math.random() * 100);
+        System.out.println("sdadsddassadasfhdhfhfdshfkjsfjkds");
+        System.out.println(randomnum);
+        int you_cannt_choudao = randomnum % 100;
+        int lantian_baiyun = you_cannt_choudao % 10;
+        if(you_cannt_choudao == 44){
+            System.out.println(you_cannt_choudao);
+            System.out.println("JIN SE CHUAN SHUO");
+        }else if(you_cannt_choudao == 45 || you_cannt_choudao == 46 || you_cannt_choudao == 47 ||
+                you_cannt_choudao == 48){
+            System.out.println(you_cannt_choudao);
+            System.out.println("kassdin");
+        }else if(lantian_baiyun == 4){
+            System.out.println(you_cannt_choudao);
+            System.out.println("karthus");
+        }else if(lantian_baiyun == 1){
+            System.out.println(you_cannt_choudao);
+            System.out.println("tydnmere");
+        }else if(lantian_baiyun == 2){
+            System.out.println(you_cannt_choudao);
+            System.out.println("garen");
+        }else if(lantian_baiyun == 3){
+            System.out.println(you_cannt_choudao);
+            System.out.println("hao");
+        }else if(lantian_baiyun == 5){
+            System.out.println(you_cannt_choudao);
+            System.out.println("wang");
+        }else if(lantian_baiyun == 6){
+            System.out.println(you_cannt_choudao);
+            System.out.println("diana");
+        }else if(lantian_baiyun == 7){
+            System.out.println(you_cannt_choudao);
+            System.out.println("mord");
+        }else{
+            System.out.println(you_cannt_choudao);
+            System.out.println("wukong");
+        }
+
         if(play.getText() == "Play"){
             VideoView videoView = findViewById(R.id.videoView);
             videoView.start();
@@ -32,5 +70,8 @@ public class Summon_Activity extends AppCompatActivity {
             play.setText("Play");
         }
 
+
     }
+
+
 }
